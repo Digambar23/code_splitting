64f8fa7d2c8a962c416ec95dc86d76bbcbf6775a
Digambar S <erdigambar@gmail.com>
2023-02-20 21:10:38 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import {lazy, React} from "react"
 // import {Home} from "./Home"
 import './App.css';
 import {useState} from "react"
+const HomePage= lazy(()=>import("./Home"));
 
 function App() {
+
   const [comp,setComp]=useState("Before Load");
   // useEffect(function(){
   //   setTimeout(function(){
@@ -14,10 +17,17 @@ function App() {
   //   },2000);
 
   // },[]);
-  const onBtnClick=async function(){
-    const resp=await import("./Home");
-    setComp(resp.Home)
+  // const onBtnClick=async function(){
+  //   const resp=await import("./Home");
+  //   setComp(resp.Home)
 
+  // }
+  const onBtnClick = function(){
+    setComp(
+      <React.Suspense>
+        <HomePage/>
+      </React.Suspense>
+    )
   }
   return (
     <div className="App"><button onClick={onBtnClick}>Go to Home</button>
